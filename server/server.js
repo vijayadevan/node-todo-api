6fa49def230a0904e2171050d10d13cc689e914b
vijayadevan <33148984+vijayadevan@users.noreply.github.com>
2017-10-30 01:22:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/user');
 var { Todo } = require('./models/todo');
 
-var Port = process.env.Port || 3000;
+var Port = process.env.PORT || 3000;
 
 var app = express();
 
