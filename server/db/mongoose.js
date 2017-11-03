d2512db9d16f391c8b568a0acdb5f946aec34d09
vijayadevan <vijayadevantheboss@gmail.com>
2017-11-04 02:01:49 +0530

    Deployed mongoDB Atlas instance

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+const uri = "mongodb://vijayadevan:LrGSdYiCvSlSNVnM@mongoplay-shard-00-00-nuszv.mongodb.net:27017,mongoplay-shard-00-01-nuszv.mongodb.net:27017,mongoplay-shard-00-02-nuszv.mongodb.net:27017/TodoApp?ssl=true&replicaSet=MongoPlay-shard-0&authSource=admin"
+
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+mongoose.connect(uri, {
     useMongoClient: true
 });
 
